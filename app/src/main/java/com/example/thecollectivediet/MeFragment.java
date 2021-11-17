c3ee38b055a11ba3945c6c8ba6d89533354f9e69
jesse <u1110728@utah.edu>
2021-11-17 15:40:52 -0700

    --Prepare for dan search pull

--- app/src/main/java/com/example/thecollectivediet/MeFragment.java
+++ app/src/main/java/com/example/thecollectivediet/MeFragment.java
@@ -4,16 +4,25 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.TableLayout;
 
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class MeFragment extends Fragment {
+
+    TabLayout tableLayout;
+    FrameLayout frameLayout;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedStateInstance){
         View v = inflater.inflate(R.layout.fragment_me, container, false);
 
-
+        tableLayout = v.findViewById(R.id.tabLayout);
+        //frameLayout = v.findViewById(R.id.include);
+        // perform setOnTabSelectedListener event on TabLayout
 
         return v;
     }
