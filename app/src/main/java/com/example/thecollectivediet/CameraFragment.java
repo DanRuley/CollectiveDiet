12d228a141331c2b73a0ff3734c0494e7d208d1d
jesse <u1110728@utah.edu>
2021-11-30 00:39:42 -0700

    --image on camera view can now be captured and put into an imageview. No bugs found at this time.

--- app/src/main/java/com/example/thecollectivediet/CameraFragment.java
+++ app/src/main/java/com/example/thecollectivediet/CameraFragment.java
@@ -1,7 +1,13 @@
 package com.example.thecollectivediet;
 
 import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.ImageFormat;
+import android.graphics.Rect;
 import android.graphics.RectF;
+import android.graphics.YuvImage;
+import android.media.Image;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.util.Size;
@@ -9,6 +15,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
 import androidx.camera.core.AspectRatio;
@@ -17,6 +24,7 @@ import androidx.camera.core.CameraControl;
 import androidx.camera.core.CameraSelector;
 import androidx.camera.core.ImageAnalysis;
 import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
 import androidx.camera.core.ImageProxy;
 import androidx.camera.core.Preview;
 import androidx.camera.lifecycle.ProcessCameraProvider;
@@ -26,6 +34,8 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.lifecycle.LifecycleOwner;
 
+import java.io.ByteArrayOutputStream;
+import java.nio.ByteBuffer;
 import java.util.List;
 import java.util.concurrent.Executors;
 
@@ -38,7 +48,12 @@ public class CameraFragment extends Fragment {
     //Buttons and views
     PreviewView previewView;
     Button takePic;
+    ImageView imageView2;
 
+    //camera
+    Preview preview;
+    CameraSelector cameraSelector;
+    ImageCapture imageCapture;
     ProcessCameraProvider cameraProvider;
     private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
 
@@ -68,16 +83,16 @@ public class CameraFragment extends Fragment {
     }
     void bindPreview(@NonNull ProcessCameraProvider cameraProvider, View view) {
 
-        Preview preview = new Preview.Builder()
+         preview = new Preview.Builder()
                 .build();
 
-        CameraSelector cameraSelector = new CameraSelector.Builder()
+         cameraSelector = new CameraSelector.Builder()
                 .requireLensFacing(CameraSelector.LENS_FACING_BACK)
                 .build();
 
         preview.setSurfaceProvider(previewView.getSurfaceProvider());
 
-        ImageCapture imageCapture =
+         imageCapture =
                 new ImageCapture.Builder()
                         .setTargetRotation(view.getDisplay().getRotation())
                         .build();
@@ -89,6 +104,64 @@ public class CameraFragment extends Fragment {
     {
         previewView = view.findViewById(R.id.view_camera);
         takePic = view.findViewById(R.id.take_pic);
+        imageView2 = view.findViewById(R.id.imageView2);
 
+        takePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                imageCapture.takePicture(ContextCompat.getMainExecutor(getActivity()), new ImageCapture.OnImageCapturedCallback(){
+                    @Override
+                    public void onCaptureSuccess(ImageProxy imageProxy){
+                        //todo
+                        //Image image = imageProxy.getImage();
+                        Bitmap bit = convertImageProxyToBitmap(imageProxy);
+
+                        imageView2.setImageBitmap(bit);
+                        //super.onCaptureSuccess(imageProxy);
+                        imageProxy.close();
+
+                    }
+                    @Override
+                    public void onError(ImageCaptureException e){
+                        //todo on error
+                        super.onError(e);
+                    }
+                });
+            }
+        });
+    }
+
+    private Bitmap convertImageProxyToBitmap(ImageProxy image) {
+        ByteBuffer byteBuffer = image.getPlanes()[0].getBuffer();
+        byteBuffer.rewind();
+        byte[] bytes = new byte[byteBuffer.capacity()];
+        byteBuffer.get(bytes);
+        byte[] clonedBytes = bytes.clone();
+        return BitmapFactory.decodeByteArray(clonedBytes, 0, clonedBytes.length);
+    }
+
+    private Bitmap toBitmap(Image image) {
+        Image.Plane[] planes = image.getPlanes();
+        ByteBuffer yBuffer = planes[0].getBuffer();
+        ByteBuffer uBuffer = planes[1].getBuffer();
+        ByteBuffer vBuffer = planes[2].getBuffer();
+
+        int ySize = yBuffer.remaining();
+        int uSize = uBuffer.remaining();
+        int vSize = vBuffer.remaining();
+
+        byte[] nv21 = new byte[ySize + uSize + vSize];
+        //U and V are swapped
+        yBuffer.get(nv21, 0, ySize);
+        vBuffer.get(nv21, ySize, vSize);
+        uBuffer.get(nv21, ySize + vSize, uSize);
+
+        YuvImage yuvImage = new YuvImage(nv21, ImageFormat.NV21, image.getWidth(), image.getHeight(), null);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        yuvImage.compressToJpeg(new Rect(0, 0, yuvImage.getWidth(), yuvImage.getHeight()), 75, out);
+
+        byte[] imageBytes = out.toByteArray();
+        return BitmapFactory.decodeByteArray(imageBytes, 0, imageBytes.length);
     }
 }
