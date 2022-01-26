18675da8c916aad993f7c0ac81e10ed9ed8916b1
jesse <u1110728@utah.edu>
2022-01-26 12:26:04 -0700

    --Added smiley rating bar but data is yet to be sent or used. Also, rating bar is not implemented programmatically yet.

--- app/src/main/java/com/example/thecollectivediet/Me_Fragment_Components/TodayFragment.java
+++ app/src/main/java/com/example/thecollectivediet/Me_Fragment_Components/TodayFragment.java
@@ -12,10 +12,12 @@ import androidx.fragment.app.FragmentTransaction;
 import com.example.thecollectivediet.Me_Fragment_Components.Food_Logging.FoodLogFragment;
 import com.example.thecollectivediet.Me_Fragment_Components.Food_Logging.ManualFoodSearch;
 import com.example.thecollectivediet.R;
+import com.hsalf.smileyrating.SmileyRating;
 
 public class TodayFragment extends Fragment {
 
     Button manualEntry;
+    SmileyRating smileyRating;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedStateInstance) {
@@ -28,6 +30,25 @@ public class TodayFragment extends Fragment {
             transaction.replace(R.id.fragmentHolder, new ManualFoodSearch());
             transaction.addToBackStack(null);
             transaction.commit();
+        });
+
+        smileyRating = v.findViewById(R.id.smile_rating);
+        smileyRating.setSmileySelectedListener(new SmileyRating.OnSmileySelectedListener() {
+            @Override
+            public void onSmileySelected(SmileyRating.Type type) {
+                // You can compare it with rating Type
+                if (SmileyRating.Type.GREAT == type) {
+                    //Log.i(TAG, "Wow, the user gave high rating");
+                }
+                // You can get the user rating too
+                // rating will between 1 to 5
+                // rating will between 1 to 5, but -1 is none selected
+                int rating = type.getRating();
+
+
+                //todo
+                //send rating out to sergio
+            }
         });
 
         return v;
