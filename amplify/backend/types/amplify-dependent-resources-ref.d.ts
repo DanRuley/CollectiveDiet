e8d7d778c3ae2e5b87d0fa1aa8ad34596c1bbf1a
jesse <u1110728@utah.edu>
2022-04-13 01:53:11 -0600

    --after git add all

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,19 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "tcdandroida874cad8": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    },
+    "storage": {
+        "S3tcd1": {
+            "BucketName": "string",
+            "Region": "string"
+        }
+    }
+}
